4e7cadf9c2419de1038598b79bbb4dac16340ea6
CheyReddy <CheyReddy@users.noreply.github.com>
2025-09-22 16:02:31 +0530

    Added new feature / fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import './App.css'
-import NavBar from './components/NavBar'
-import Container from './components/Container'
-import { Route, Routes } from 'react-router-dom'
-import Counter from './components/Counter'
-import ImageGallery from './components/ImageGallery'
-import Accordion from './components/Accordion'
-import RandomNumberGenerator from './components/RandomNumberGenerator'
-import TelephoneFormatter from './components/TelephoneFormatter'
-import StringTransformers from './components/StringTransformers'
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Container from "./components/Container";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Counter from "./components/Counter";
+import ImageGallery from "./components/ImageGallery";
+import Accordion from "./components/Accordion";
+import RandomNumberGenerator from "./components/RandomNumberGenerator";
+import TelephoneFormatter from "./components/TelephoneFormatter";
+import StringTransformers from "./components/StringTransformers";
 
 function App() {
-
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<Container />} />
-        <Route path='/counter' element={<Counter />} />
-        <Route path='/image-gallery' element={<ImageGallery />} />
-        <Route path='/accordion' element={<Accordion />} />
-        <Route path='/random-number-generator' element={<RandomNumberGenerator />} />
-        <Route path='/telephone-formatter' element={<TelephoneFormatter />} />
-        <Route path='/string-transformers' element={<StringTransformers />} />
-      </Routes>
-        
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Container />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/image-gallery" element={<ImageGallery />} />
+          <Route path="/accordion" element={<Accordion />} />
+          <Route
+            path="/random-number-generator"
+            element={<RandomNumberGenerator />}
+          />
+          <Route path="/telephone-formatter" element={<TelephoneFormatter />} />
+          <Route path="/string-transformers" element={<StringTransformers />} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
